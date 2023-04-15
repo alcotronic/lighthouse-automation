23a92907a7ad54c84574ef1279e5022262d01264
alcotronic <alcotronic@gmx.de>
2023-04-15 14:39:22 +0200

    initial commit with basic nestjs backend and config

---
+++ apps/lha-backend/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'apps/lha-backend/.development.env',
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: +configService.get('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        dbName: configService.get<string>('MONGODB_DBNAME'),
+        user: configService.get<string>('MONGODB_DB_USER'),
+        pass: configService.get<string>('MONGODB_DB_PASSWORD'),
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_SIGN_OPTIONS_EXPIRES_IN'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
