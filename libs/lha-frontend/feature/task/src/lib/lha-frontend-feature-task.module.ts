700e8b921855f7710196b8e73e4ff3e2b55b17d6
alcotronic <alcotronic@gmx.de>
2023-04-17 20:57:23 +0200

    fixed bar chart for task executions

--- libs/lha-frontend/feature/task/src/lib/lha-frontend-feature-task.module.ts
+++ libs/lha-frontend/feature/task/src/lib/lha-frontend-feature-task.module.ts
@@ -4,10 +4,18 @@ import { TaskComponent } from './task/task.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { NgChartsModule } from 'ng2-charts';
 import { LhaFrontendFeatureTaskExecutionListModule } from '@lighthouse-automation/lha-frontend/feature/task-execution-list';
 
 @NgModule({
-  imports: [CommonModule, MatCardModule, MatIconModule, MatListModule, LhaFrontendFeatureTaskExecutionListModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    NgChartsModule,
+    LhaFrontendFeatureTaskExecutionListModule,
+  ],
   declarations: [TaskComponent, TaskComponent],
   exports: [TaskComponent],
 })
