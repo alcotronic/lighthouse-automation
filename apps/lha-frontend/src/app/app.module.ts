50cf4a9d9d4d6a62047f86cf31e6d9430b32d8ca
alcotronic <alcotronic@gmx.de>
2023-04-26 06:13:47 +0200

    added ngrx to frontend app

--- apps/lha-frontend/src/app/app.module.ts
+++ apps/lha-frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, isDevMode } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +22,9 @@ import { TaskModule } from './task/task.module';
 import { LhaFrontendDataAccessTaskModule } from '@lighthouse-automation/lha-frontend/data-access/task';
 import { LhaFrontendDataAccessTaskExecutionModule } from '@lighthouse-automation/lha-frontend/data-access/task-execution';
 import { LhaFrontendDataAccessReportModule } from '@lighthouse-automation/lha-frontend/data-access/report';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -56,6 +59,24 @@ registerLocaleData(localeDE);
     LhaFrontendFeatureSetupModule,
     LhaFrontendFeatureToolbarModule,
     TaskModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+    EffectsModule.forRoot([]),
   ],
   providers: [
     {
